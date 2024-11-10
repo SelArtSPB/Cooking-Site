e79131e96cc77264a9e5da2f577bf2d6a2e1272f
unknown <you@example.com>
2024-11-10 10:07:58 +0300

    10.11

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,5 @@
 <script>
-    document.addEventListener("DOMContentLoaded", function() {
-    const firinBanners = document.querySelectorAll('.firin-banner');
-    const secinBanners = document.querySelectorAll('.secin-banner');
-    let firinIndex = 0;
-    let secinIndex = 0;
-
-    function showNextBanner(banners, index) {
-    banners.forEach((banner, i) => {
-    banner.style.opacity = i === index ? '1' : '0';
-});
-}
-
-    function startCarousel() {
-    showNextBanner(firinBanners, firinIndex);
-    showNextBanner(secinBanners, secinIndex);
-
-    firinIndex = (firinIndex + 1) % firinBanners.length;
-    secinIndex = (secinIndex + 1) % secinBanners.length;
-}
-
-    setInterval(startCarousel, 3000);
+    document.querySelector('.menu-toggle').addEventListener('click', function () {
+    document.querySelector('.nav ul').classList.toggle('active')
 });
 </script>
